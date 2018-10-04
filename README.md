c66d887646970a4fcfefd0b16c3b7ca1287fb63a
Milton Bravo <miltonbravo96@gmail.com>
2018-10-03 19:46:45 -0500

    modificado el package json y app.js

--- README.md
+++ README.md
@@ -2,4 +2,5 @@
 
 Actualmente solo se ha iniciado
 
+Antes de inicia el proyecto se deben instalar los modulos del package json: npm install
 Para iniciar el proyecto usar: SET DEBUG=chat:* & npm start
--- app.js
+++ app.js
@@ -21,6 +21,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.get('/chatingo',function(req, res){
+  res.render('chatingo');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
